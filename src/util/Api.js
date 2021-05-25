404717a3130ad5c8742edfa66aa5767266e0121b
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-05-25 22:48:29 +0900

    https://

--- src/util/Api.js
+++ src/util/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: 'http://ec2-3-37-73-159.ap-northeast-2.compute.amazonaws.com',
+    baseURL: 'https://ec2-3-37-73-159.ap-northeast-2.compute.amazonaws.com',
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
