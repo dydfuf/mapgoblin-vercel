a62c0c9d705ae3d474ff7ab44401a18a0bea69aa
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-06-06 21:43:58 +0900

    base url change

--- src/util/Api.js
+++ src/util/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://www.mapgoblin.kro.kr/api',
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
